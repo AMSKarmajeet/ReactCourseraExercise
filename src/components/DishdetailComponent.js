70adec2d5b7e250511a3ba570970c14ccf5b1fe1
AMSKarmajeet <kcbhakta.99@gmail.com>
2020-09-18 18:01:18 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React , { Component } from 'react'
 import { Card, CardImg,CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Button, Row, Col, Label} from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent'
 
 
 const required = val => val && val.length;
@@ -155,10 +156,25 @@ function RenderDish({dish}) {
 
 
 const  DishDetail = (props) => {
-
-    const dish = props.dish
-        if(dish === null || dish === undefined) return (<div></div>)
-
+  if (props.isLoading) {
+    return(
+      <div className="container">
+          <div className="row">
+              <Loading />
+          </div>
+      </div>
+    );
+  }
+  else if (props.errMess) {
+    return(
+      <div className="container">
+          <div className="row">
+              <h4>{props.errMess}</h4>
+          </div>
+      </div>
+    );
+  }
+  else if (props.dish != null)
     return (
         <div className = 'container'>
 
@@ -181,10 +197,13 @@ const  DishDetail = (props) => {
                         addComment={props.addComment}
                         dishId={props.dish.id} />
                 </div>
-
         </div>
     );
-
+  else {
+    return(
+      <div></div>
+    );
+  }
   }
 
-export default DishDetail
+export default DishDetail;
