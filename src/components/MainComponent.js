5ad489749be68a4c12c4f63418c0d0e505b8417b
AMSKarmajeet <kcbhakta.99@gmail.com>
2020-09-18 14:34:38 +0530

    Combining Reducer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-
-
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
@@ -25,7 +23,6 @@ const mapStateToProps = state => {
 class Main extends Component {
   constructor(props) {
     super(props);
-
   }
 
   onDishSelect(dishId) {
