7a1d21920942d2d2eaa088076efedf8d1b4691c7
AMSKarmajeet <kcbhakta.99@gmail.com>
2020-09-19 09:55:28 +0530

    Fetched from the Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,7 +2,8 @@ import React , { Component } from 'react'
 import { Card, CardImg,CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Button, Row, Col, Label} from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from "react-redux-form";
-import { Loading } from './LoadingComponent'
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = val => val && val.length;
@@ -120,7 +121,7 @@ function RenderDish({dish}) {
     return (
         <div className = 'col-12 col-md-5 m-1'>
 						<Card>
-							<CardImg width="100%" src={dish.image} alt={dish.name}/>
+							<CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
 							<CardBody>
 								<CardTitle>{dish.name}</CardTitle>
 								<CardText>{dish.description}</CardText>
